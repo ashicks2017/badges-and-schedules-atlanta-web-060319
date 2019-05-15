ea58b15c47acacd0e2508908255421b6db5e39dc
aeisha simmons <ashicks2017@outlook.com>
2019-05-15 22:38:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,13 +14,9 @@ end
 
 
 def assign_rooms(array)
-new_array = []
-    counter = l
-    array.each do |name|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-      counter += 1
-    end
-  return new_array
+  new_array = []
+  counter = l
+  array.
 end
 
 
