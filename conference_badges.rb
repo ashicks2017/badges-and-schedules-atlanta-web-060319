2d3d88c6089224669a998e68e7fb6f0f8b4bcf7d
aeisha simmons <ashicks2017@outlook.com>
2019-05-15 22:21:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,22 +5,20 @@ end
 
 
 def batch_badge_creator (array)
-new_array = []
-array.each do |name|
-new_array.push("Hello, my name is #{name}.")
+  new_array = []
+  array.each do |name|
+  new_array.push("Hello, my name is #{name}.")
+end
+  return new_array
+end
+
+
+def assign_rooms(array)
+ new_array = []
+ counter = l 
+ array.each do |name|
+ new_array.push("Hello #{name}! You'll be assigned to room #{counter})!")
+ counter += 1  
 end
  return new_array
 end
-
-
-def assign_rooms(speaker,counter)
- counter = l 
-end
-
-#def brother_meth(brother, counter)
-#counter = 1
-#brother.each do |brother_name|
- # puts "This is loop number #{counter}"
-##puts "Stop hitting yourself #{brother}!"
-  # += 1
-#end
