d0de833501732be7a1fc95211efa03244c7ee390
aeisha simmons <ashicks2017@outlook.com>
2019-05-15 22:04:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,15 +4,12 @@ def badge_maker(name)
 end
 
 
-def batch_badge_creator (array)
- new_array = []
- array.each do |name|
- new_array.push("Hello, my name is #{name}.")
-end
- return new_array
-end
-
-
-def assign_rooms
-  
-end
+#def batch_badge_creator (array)
+# new_array = []
+# array.each do |name|
+# new_array.push("Hello, my name is #{name}.")
+#end
+# return new_array
+#end
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus"]
+def batch_badge_creator()
